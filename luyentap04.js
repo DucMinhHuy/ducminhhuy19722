4ef5e48c2d8a68a40f97edb96a76b5bc95eabc29
Ducminhhuy <ducminhhuy99tbvn2@gmail.com>
2022-07-19 17:23:31 +0700

    197

---
+++ luyentap04.js
@@ -0,0 +1,13 @@
+// let length=+prompt("enter vao do dai mang");
+// let array = new Array(length);
+// for(let i=0;i<array.length;i++)
+//     {
+//         let value = +prompt("enter vao gia tri" + (i + 1));
+//         document.write(value);
+//     }
+//     value.reverse(value);
+//
+// document.write(value);
+let array=[2,3,4,5,6];
+array.reverse();
+console.log(array);
---
+++ test.13.js
@@ -0,0 +1,16 @@
+// let x=[2,4,5];
+// let counter =0;
+// if(x[counter]<5){
+//     console.log("code");
+// }else{
+//     console.log("gym")
+// }
+
+
+let x=[1,3,5,8];
+for(let i=0;i<x.length;i++){
+    if(x[i]>=3){
+        x[i]=0
+    }
+}
+console.log(x)
